f939731e0cae8d95853d2535632458e3603c26ed
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-09 18:00:57 +0530

    create list components

---
+++ frontend/src/components/Explore/Explore.jsx
@@ -0,0 +1,65 @@
+import "./explore.css";
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/region/square250/49646.webp?k=b7f38878b9164ee38e0b99c4d4646dbea76b7bf4add8464b1aa75e4c9d0efc6e&o="
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Goa</h1>
+          <h2>5,247 properties</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/region/square250/60943.webp?k=764589bec5bc72ca0abf6d79f8e86bc0a992d9a9a13a67c1c5d0a196de947077&o="
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Grand Canyon</h1>
+          <h2>3,906 properties</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/region/square250/67964.webp?k=8f4f2ee5df4d880fe7f202927984f81655afbda90b7682a8f0582a0fcaa5e284&o="
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Georgia</h1>
+          <h2>1,374 properties</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/region/square250/68431.webp?k=b819d986ecd87c0e182f4ddd3dcadeaa271e3b308061040bb86f5658a0d0340a&o="
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>New Jersey</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/region/square250/68085.webp?k=0f810fd728bfd94157ce21f611e004eb6fa47a2722067e8086cd5e5f6bba24b9&o="
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Grand National Park</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,9 +2,12 @@ import "./home.css";
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from "../../components/featured/Featured";
+import Explore from "../../components/Explore/Explore";
 import PropertyList from "../../components/propertyList/PropertyList";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
+import { Connect } from "../../components/Connect/Connect";
 
 const Home = () => {
     return (
@@ -12,12 +15,18 @@ const Home = () => {
             <Navbar/>
             <Header/>
             <div className="homeContainer">
-            <Featured/>
+           
+            <h1 className="homeTitle">Top destinations</h1>
+            <Explore/>
             <h1 className="homeTitle">Browse by property type</h1>
             <PropertyList/>
+            <Featured/>
             <h1 className="homeTitle">Homes guests love</h1>
             <FeaturedProperties/>
-            <MailList/>
+            <h1 className="homeTitle" >Connect with other travelers</h1>
+            <Connect/>
+            <MailList/>         
+            <Footer/>
             </div>
         </div>
     );
