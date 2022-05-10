01170929a567b2ac5d22579e7be69a55323a828c
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-10 15:30:20 +0530

    create login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import {
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Login/Signup";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/hotels" element={<List/>}/>
       <Route path="/hotels/:id" element={<Hotel/>}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/Signup" element={<Signup />}/>
       </Routes>
     </BrowserRouter>
   );
