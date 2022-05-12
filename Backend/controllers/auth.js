fbdf57697fd6ec0149c9a95b4cda164f9c3ed2f1
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-12 10:35:14 +0530

    generate jwt and store in cookies

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
     try {
@@ -32,9 +33,19 @@ export const login = async (req, res, next) => {
 
         return next(createError(400, "Wrong password or username!"));
 
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.JWT
+          );
+          
         const { password, isAdmin, ...otherDetails } = user._doc;
         
-        res.status(200).send({ ...otherDetails} );
+        res
+        .cookie("access_token", token, {
+          httpOnly: true,   //doesnt allow to clientside to access a token and we stok=re token in cookies
+        })
+        .status(200)
+        .json({ ...otherDetails });
     } catch (err) {
         next(err);
     }
